63c13d19dcae4efab218b7d8fa53fc2d7b6138a2
anjan7382 <129925119+anjan7382@users.noreply.github.com>
2025-12-15 12:51:56 +0530

    Refactor variable definitions in varible.tf
    Updated variable defaults and renamed some variables.

--- varible.tf
+++ varible.tf
@@ -1,47 +1,21 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
 
 variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-  default     = "demo-eks"
+  default = "prod-eks-cluster"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "node_instance_types" {
-  description = "Instance types for worker nodes"
-  type        = list(string)
-  default     = ["t3.medium"]
+variable "private_subnets" {
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
-variable "desired_size" {
-  description = "Desired number of worker nodes"
-  type        = number
-  default     = 2
-}
 
-variable "min_size" {
-  description = "Minimum number of worker nodes"
-  type        = number
-  default     = 1
-}
-
-variable "max_size" {
-  description = "Maximum number of worker nodes"
-  type        = number
-  default     = 3
-}
