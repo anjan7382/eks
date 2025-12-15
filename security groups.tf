ae5c5ee3041ad1b32d62803cb672d80247c4603d
anjan7382 <129925119+anjan7382@users.noreply.github.com>
2025-12-15 12:53:57 +0530

    Refactor EKS security group settings
    Updated security group configuration for EKS cluster.

--- security groups.tf
+++ security groups.tf
@@ -1,29 +1,20 @@
-(Security group for the EKS control plane)
-
 resource "aws_security_group" "eks_cluster_sg" {
-  name        = "${var.cluster_name}-cluster-sg"
-  description = "Security group for EKS cluster control plane"
-  vpc_id      = aws_vpc.this.id
+  name   = "eks-cluster-sg"
+  vpc_id = aws_vpc.eks.id
 
-  # Allow kubectl/API access from anywhere (for testing)
-  # In real-time, restrict this to your office IP / VPN.
   ingress {
-    description = "Allow EKS API (HTTPS)"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # All outbound
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "${var.cluster_name}-cluster-sg"
-  }
 }
+
+
