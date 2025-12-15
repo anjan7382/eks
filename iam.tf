4d1293cacb6a8413af0260ab8f9d668c3e8195bc
anjan7382 <129925119+anjan7382@users.noreply.github.com>
2025-12-15 13:17:02 +0530

    Add IAM roles and policies for EKS control plane and nodes

---
+++ iam.tf
@@ -0,0 +1,61 @@
+############################################
+# 1️⃣ EKS CONTROL PLANE IAM ROLE
+############################################
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  # 👇 WHO CAN ASSUME THIS ROLE?
+  # Only the EKS service (NOT EC2)
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Required policy for EKS control plane
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+############################################
+# 2️⃣ WORKER NODE (EC2) IAM ROLE
+############################################
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  # 👇 WHO CAN ASSUME THIS ROLE?
+  # Only EC2 instances (worker nodes)
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Worker node permissions (MANDATORY)
+resource "aws_iam_role_policy_attachment" "node_worker_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_ecr_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
