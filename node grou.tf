d9adeeb097317243dd0d9b4a7ece91797a2c1515
anjan7382 <129925119+anjan7382@users.noreply.github.com>
2026-01-07 12:46:46 +0530

    Refactor EKS node group IAM role and dependencies
    Updated the node group configuration to use the new IAM role and added dependency on IAM role policies.

--- node grou.tf
+++ node grou.tf
@@ -1,31 +1,9 @@
-resource "aws_iam_role" "node_role" {
-  name = "eks-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  ])
-  role       = aws_iam_role.node_role.name
-  policy_arn = each.value
-}
 
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "prod-nodes"
-  node_role_arn  = aws_iam_role.node_role.arn
-  subnet_ids     = aws_subnet.private[*].id
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
     desired_size = 2
@@ -34,9 +12,8 @@ resource "aws_eks_node_group" "nodes" {
   }
 
   instance_types = ["t3.medium"]
-}
 
-#Creates EC2 instances
-#Uses worker-node IAM role from iam.tf
-#Without this → NO NODES, NO PODS
-#👉 Node group = hands (workers)
+  depends_on = [
+    aws_iam_role_policy_attachment.node_policies
+  ]
+}
