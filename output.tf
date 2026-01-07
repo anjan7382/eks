d5680f3481e090952a3f4fde6a433aad974b5582
anjan7382 <129925119+anjan7382@users.noreply.github.com>
2026-01-07 12:49:45 +0530

    Add outputs for cluster endpoint and private subnets

--- output.tf
+++ output.tf
@@ -2,6 +2,14 @@ output "cluster_name" {
   value = aws_eks_cluster.eks.name
 }
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
 output "vpc_id" {
   value = aws_vpc.eks.id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
 }
