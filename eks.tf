acd9dd8c776cad1849ab10f309e5239bc5264400
anjan7382 <129925119+anjan7382@users.noreply.github.com>
2026-03-12 12:38:56 +0530

    Refactor EKS cluster resource to use module

--- eks.tf
+++ eks.tf
@@ -1,17 +1,41 @@
-resource "aws_eks_cluster" "eks" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+module "eks" {
 
-  vpc_config {
-    subnet_ids = concat(
-      aws_subnet.public[*].id,
-      aws_subnet.private[*].id
-    )
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.cluster_name
+  kubernetes_version = var.kubernetes_version
+
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.private_subnets
+  control_plane_subnet_ids = var.control_plane_subnets
+
+  addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
+  eks_managed_node_groups = {
+
+    workers = {
+
+      instance_types = [var.instance_type]
+      ami_type       = "AL2023_x86_64_STANDARD"
+
+      min_size     = var.min_size
+      max_size     = var.max_size
+      desired_size = var.desired_size
+
+    }
+
+  }
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+
 }
 
 
